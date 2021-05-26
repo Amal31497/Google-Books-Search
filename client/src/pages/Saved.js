ff7db2cd7e93c0f03d5c0488d74b98284542890d
Amal Janabayev <amalj2426@gmail.com>
2021-05-26 13:34:23 -0700

    Fixed basic formatting and styling for the Saved page, next work on front end API routes

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchTitle from '../components/SearchTitle';
+import BookSearch from '../components/BookSearch';
+import ResultsBox from '../components/ResultsBox';
+
+
+
+function Saved() {
+
+    return (
+        <div>
+            <SearchTitle />
+            <ResultsBox />
+        </div>
+    )
+}
+
+export default Saved;
