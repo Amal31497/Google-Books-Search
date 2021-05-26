d3ecc4d2c5b06ed105e5398aa22dd67732785f5e
Amal Janabayev <amalj2426@gmail.com>
2021-05-26 14:23:08 -0700

    Created front end API functions in utils folder

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { Card, Button, InputGroup } from 'react-bootstrap';
+import API from '../../../utils/API'
 import './style.css';
 
 function BookSearch(props) {
 
     const [search, setSearch] = useState("")
 
-
+    const searchBook = (search){
+        
+    }
 
     return (
         <Card id="searchBox" style={{justifyContent:"space-evenly", alignItems:'center'}}>
---
+++ client/utils/API.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const APIKEY = "key=AIzaSyAyS5JORNcOvvGYQeJgQT87aWIcOY04ad4";
+
+export default {
+
+    // ------ Search Page ------
+
+    search: function (query) {
+        return axios.get(BASEURL + query + APIKEY);
+    },
+
+    // Saves a book to the database
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData);
+    },
+
+    // ------ Saved Page ------
+
+    getBooks: function () {
+        return axios.get("/api/books");
+    },
+
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id);
+    }
+};
